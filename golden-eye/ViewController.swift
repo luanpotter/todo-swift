508f9d9c0d7f009dc5f99a87d9b6e1f5253fbdb8
Luan Nico <luannico27@gmail.com>
2017-08-05 17:01:33 -0300

    todo app

--- golden-eye/ViewController.swift
+++ golden-eye/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var txt: UITextField!
+    @IBOutlet weak var table: UITableView!
+
+    var items = [String]() {
+        didSet {
+            table.reloadData()
+        }
+    }
+    
+    @IBAction func click(_ sender: Any) {
+        if let str = txt.text {
+            items.append(str)
+            txt.text = ""
+            txt.becomeFirstResponder()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        table.dataSource = self
+        table.delegate = self
+        txt.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init()
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            items.remove(at: indexPath.row)
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        click(textField)
+        return true
+    }
 }
 
